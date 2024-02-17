640f5992cc20abe950e1aa257c467772520ad328
Yasmin Fragomeni <yasmin.fragomeni@willowtreeapps.com>
2024-02-16 21:58:52 -0300

    Setup real project!

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import type { InlineConfig } from "vitest";
+import { defineConfig, type UserConfig } from "vite";
+
+import react from "@vitejs/plugin-react";
+
+type ViteConfig = UserConfig & { test: InlineConfig };
+const config: ViteConfig = {
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/test/setup.js",
+  },
+};
+export default defineConfig(config);
